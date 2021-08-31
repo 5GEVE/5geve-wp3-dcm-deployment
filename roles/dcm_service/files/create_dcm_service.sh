9077f8602b98d1e0e40907a58aaa13303fedc5bd
Matteo Pergolesi <matpergo@gmail.com>
2021-08-31 17:17:24 +0200

    Put every Kafka mirror in a systemd unit for better management.

--- roles/dcm_service/files/create_dcm_service.sh
+++ roles/dcm_service/files/create_dcm_service.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-echo [Unit] | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
-echo Description=DCM REST Client | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+# TODO we should change this so it reflects latest changes
+
+echo "[Unit]" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "Description=DCM REST Client" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "Requires=kafka-mirror-french-site.service kafka-mirror-greek-site.service kafka-mirror-italian-site.service kafka-mirror-spanish-site.service" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "After=kafka-mirror-french-site.service kafka-mirror-greek-site.service kafka-mirror-italian-site.service kafka-mirror-spanish-site.service" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
 echo | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
-echo [Service] | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
-echo User=$1 | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
-echo ExecStart=/usr/bin/sudo /usr/bin/python3 /usr/bin/dcm/dcm-python/dcm_rest_client.py --dcm_ip_address $2 --spanish_site_plugin_ip_port $3 --italian_site_plugin_ip_port $4 --french_site_plugin_ip_port $5 --greek_site_plugin_ip_port $6 --port 8090 --log info | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "[Service]" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "User=$1" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "ExecStart=/usr/bin/sudo /usr/bin/python3 /usr/bin/dcm/dcm-python/dcm_rest_client.py --dcm_ip_address $2 --spanish_site_plugin_ip_port $3 --italian_site_plugin_ip_port $4 --french_site_plugin_ip_port $5 --greek_site_plugin_ip_port $6 --port 8090 --log info" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "TimeoutSec=10" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "Restart=on-failure" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "RestartSec=1" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
 echo | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
-echo [Install] | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
-echo WantedBy=default.target | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "[Install]" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
+echo "WantedBy=default.target" | sudo tee -a /etc/systemd/system/dcm.service > /dev/null
 
 sudo chmod 664 /etc/systemd/system/dcm.service
 sudo systemctl daemon-reload
--- roles/preparation/files/create_kafka_mirror.sh
+++ roles/preparation/files/create_kafka_mirror.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
-# Arguments:
-# 1-site
-# 2-topic name
+#!/usr/bin/env bash
 
 SITE=$1
-TOPIC=$2
 
-/opt/kafka/bin/kafka-run-class.sh kafka.tools.MirrorMaker --consumer.config /usr/bin/dcm/${SITE}_consumer.config --num.streams 1 --producer.config /usr/bin/dcm/producer.config --whitelist \'${TOPIC}\' &
+/opt/kafka/bin/kafka-run-class.sh \
+  kafka.tools.MirrorMaker \
+    --abort.on.send.failure "true" \
+    --consumer.config "/usr/bin/dcm/${SITE}_consumer.config" \
+    --num.streams 4 \
+    --producer.config /usr/bin/dcm/producer.config \
+    --whitelist '^.*\.application_metric\..*$,^.*\.infrastructure_metric\..*$'
